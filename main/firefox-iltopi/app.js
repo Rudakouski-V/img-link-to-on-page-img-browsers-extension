57b3c56a9136151a4b4dbdaa34d54688cc9b57ae
Rudakouski_V <rva1997@gmail.com>
2023-04-10 22:27:52 +0300

    changed search logic and manifest, added test

--- main/firefox-iltopi/app.js
+++ main/firefox-iltopi/app.js
@@ -1,22 +1,27 @@
 (function () {
+    //const googleRegexp = /^(http|https):\/{2}\w.+google.com\/[\w\/]+.\w+/;
+    const userapiComRegexp = /^(http|https):\/{2}sun\d+-\d+.userapi.com\/imp(f|g)\//;
     const formats = ['jpeg', 'jpg', 'png', 'webp'];
     let links = document.querySelectorAll('a[href]:not([itemprop])');
 
     links.forEach(function (link) {
-        let url = link.getAttribute('href');
-        let ext = _getUrlExt(url);
+        if (userapiComRegexp.test(link.toString())) {
+            let url = link.getAttribute('href');
+            let ext = _getUrlExt(url);
 
-        if (formats.includes(ext)) {
-            let image = document.createElement("img");
-            image.setAttribute('class', 'iltopi');
-            image.setAttribute('src', url);
-            link.innerHTML = "";
-            link.appendChild(image);
-            link.setAttribute('target', '_blank');
+            if (formats.includes(ext)) {
+                let image = document.createElement("img");
+                image.setAttribute('class', 'iltopi');
+                image.setAttribute('src', url);
+                link.innerHTML = "";
+                link.appendChild(image);
+                link.setAttribute('target', '_blank');
+            }
         }
     });
 
     function _getUrlExt(url) {
-        return url.split('.').pop();
+        let possibleUrl = url.split('.').pop();
+        return possibleUrl.split('?')[0];
     }
 })();
